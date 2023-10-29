30ccf0056ba269370f3b2b0396d4eb380eb73308
neazxeon <neaz@xeon.email>
2023-10-29 11:12:02 +0600

    signin with google backend

--- front/src/components/OAuth.jsx
+++ front/src/components/OAuth.jsx
@@ -1,32 +1,35 @@
 import React from "react";
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
-import {app} from '../firebase.js'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase.js";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../Redux/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
-    const dispatch=useDispatch()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try {
-        const provider=new GoogleAuthProvider() //This is the first step for for google auth
-        const auth=getAuth(app)// Then we pass the google auth of our credentials which is in firebase
-        const result=await signInWithPopup(auth,provider)//This fire the pop up window for signin with login I
-        console.log(result)
-        const res = await fetch('/api/auth/google', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              name: result.user.displayName,
-              email: result.user.email,
-              photo: result.user.photoURL,
-            }),
-          });
-          const data = await res.json();
-          dispatch(signInSuccess(data));
-        console.log('data====>',data)
-        dispatch(signInSuccess(data))
+      const provider = new GoogleAuthProvider(); //This is the first step for for google auth
+      const auth = getAuth(app); // Then we pass the google auth of our credentials which is in firebase
+      const result = await signInWithPopup(auth, provider); //This fire the pop up window for signin with login I
+      console.log(result);
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      console.log("data====>", data);
+      dispatch(signInSuccess(data));
+      navigate('/');
     } catch (error) {
       console.log("could not sign in with google", error);
     }
