71309b1c8f18a812a88dc30d333c7623aa8ea794
neazxeon <neaz@xeon.email>
2023-10-29 11:11:58 +0600

    signin with google backend

--- api/index.js
+++ api/index.js
@@ -25,7 +25,6 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-
 //This is a Middleware
 app.use((err, req, res, next) => {
   const statuscode = err.statusCode || 500;
